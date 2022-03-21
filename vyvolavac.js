e405f2bacfbd4eb5807bcf986843e1ffa258cbe6
helenaKuthanova <helena.kuthanova@gmail.com>
2022-03-21 18:11:23 +0100

    doplnen bonus pro holky

--- vyvolavac.js
+++ vyvolavac.js
@@ -28,6 +28,7 @@ let jmena = [
 ];
 
 let tazenaJmena = [];
+let pocitadlo = 0;
 
 function tahnoutJmeno() {
     if (jmena.length === 0) {
@@ -44,11 +45,36 @@ function tahnoutJmeno() {
     jmena.splice(vyherniIndex, 1);
 
     // Výherní jméno si uložíme do pole k ostatním výherním
-    tazenaJmena.push(vyherniJmeno);
+    tazenaJmena.unshift(vyherniJmeno);
 
     let zapisVyhernihoJmena = document.querySelector("#vyherka");
     zapisVyhernihoJmena.textContent = vyherniJmeno;
 
     let seznamVyhercu = document.querySelector("#seznam");
     seznamVyhercu.textContent = tazenaJmena;
+
+    pocitadlo = tazenaJmena.length;
+    console.log(pocitadlo)
+    let pocetVyhercu = document.querySelector("#pocet");
+    pocetVyhercu.textContent = pocitadlo;
+}
+
+function restart() {
+    // tažená jména vrátím do seznamu jmen
+    tazenaJmena.forEach(function(ele) {
+        jmena.push(ele);
+    });
+
+    // tažená jména a počitadlo vyprázdním
+    tazenaJmena = [];
+    console.log(jmena);
+
+    let pocetVyhercu = document.querySelector("#pocet");
+    pocetVyhercu.textContent = 0;
+
+    let seznamVyhercu = document.querySelector("#seznam");
+    seznamVyhercu.textContent = "";
+
+    let zapisVyhernihoJmena = document.querySelector("#vyherka");
+    zapisVyhernihoJmena.textContent = "";
 }
